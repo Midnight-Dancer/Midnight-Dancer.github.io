1505721166d247f8918e001b0b2d5db55396c30d
Midnight-Dancer <129922109+Midnight-Dancer@users.noreply.github.com>
2023-04-06 05:01:12 +0200

    Update date.js

--- date.js
+++ date.js
@@ -2,5 +2,5 @@ setInterval(myTimer, 1000);
 
 function myTimer() {
   const date = new Date();
-  document.getElementById("date").innerHTML = date.toLocaleDateString();
+  document.getElementById("date").innerHTML = date.toLocaleDateString("en-GB");
 }
